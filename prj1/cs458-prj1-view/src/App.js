a9953c67bfdf9a65d5296391ec5feaf57b3818fa
Talha <talhaburakcuhadar@gmail.com>
2021-02-09 11:26:33 +0300

    basit structure

--- prj1/cs458-prj1-view/src/App.js
+++ prj1/cs458-prj1-view/src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home/home';
+import SignIn from './pages/sign-in/signin';
+import SignUp from './pages/sign-up/signup';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/sign-in">
+            <SignIn />
+          </Route>
+          <Route path="/sign-up">
+            <SignUp />
+          </Route>
+        </Switch>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sign-in">About</Link>
+          </li>
+          <li>
+            <Link to="/sign-up">Dashboard</Link>
+          </li>
+        </ul>
+      </div>
+      
+    </Router>
   );
 }
 
---
+++ prj1/cs458-prj1-view/src/pages/sign-in/signin.js
@@ -0,0 +1,7 @@
+export default function SignIn() {
+    return (
+      <div>
+        <h2>SignIn</h2>
+      </div>
+    );
+  }
