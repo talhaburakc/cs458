0c74938441109cda957023414b218669195de8d4
Perman <perman.atayev17@gmail.com>
2021-02-09 17:29:46 +0300

    signup2 changes

--- prj1/cs458-prj1-view/src/pages/sign-up/signup_2.js
+++ prj1/cs458-prj1-view/src/pages/sign-up/signup_2.js
@@ -2,50 +2,85 @@ import React from 'react';
 import {Button, Form} from 'react-bootstrap'
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import {useHistory, withRouter} from "react-router-dom";
 
-export default function Signup_2() {
 
-    return (
-        <div>
-            <Header/>
+class Signup_2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state.email = "";
+    }
 
-            <div style={{marginTop: "15px"}}>
-                <div className="w-25" style={{margin: "auto"}}>
-                    <div>
-                        <span id="" className="stepIndicator" data-uia="">STEP <b>1</b> OF <b>3</b></span>
-                        <h1 className="stepTitle" data-uia="stepTitle">Create a password to start your membership.</h1>
-                    </div>
 
-                    <div>
-                        <div className="contextRow" data-uia="contextRowDone">Just a few more steps and you're done!
+    handleEmailChange = (e) => {
+        this.setState({email: e.target.value});
+    }
+    handlePasswordChange = (e) => {
+        this.setState({password: e.target.value});
+    }
+    handleCheckBoxChange = (e) => {
+        this.setState({checkbox: e.target.value});
+    }
+
+    handleContinueClick = (e) => {
+        // this.setState({password: e.target.value});
+        let path = `/sign-up3`;
+        console.log(this.state.email)
+        console.log(this.state.password)
+
+        // const {history} = this.props;
+        //
+        // history.push(path);
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+
+                <div style={{marginTop: "15px"}}>
+                    <div className="w-25" style={{margin: "auto"}}>
+                        <div>
+                            <span id="" className="stepIndicator" data-uia="">STEP <b>1</b> OF <b>3</b></span>
+                            <h1 className="stepTitle" data-uia="stepTitle">Create a password to start your
+                                membership.</h1>
                         </div>
-                        <div className="contextRow" data-uia="contextRowPaperWork">We hate paperwork, too.</div>
-                        <br/>
 
-                        <Form>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email"/>
-                            </Form.Group>
+                        <div>
+                            <div className="contextRow" data-uia="contextRowDone">Just a few more steps and you're done!
+                            </div>
+                            <div className="contextRow" data-uia="contextRowPaperWork">We hate paperwork, too.</div>
+                            <br/>
 
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password"/>
-                            </Form.Group>
-                            <Form.Group controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Please do not email me Netflix special offers."/>
-                            </Form.Group>
-                            <Button variant="danger" type="submit" className="w-100">
-                                Continue
-                            </Button>
-                        </Form>
+                            <Form>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" value={this.state.email}
+                                                  onChange={this.handleEmailChange}/>
+                                </Form.Group>
+
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" value={this.state.password}
+                                                  onChange={this.handlePasswordChange}/>
+                                </Form.Group>
+                                <Form.Group controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Please do not email me Netflix special offers."
+                                                value={this.state.checkbox} onChange={this.handleCheckBoxChange}/>
+                                </Form.Group>
+                                <Button variant="danger" type="submit" className="w-100" onClick={this.handleContinueClick}>
+                                    Continue
+                                </Button>
+                            </Form>
+                        </div>
                     </div>
+
                 </div>
 
+                <Footer/>
             </div>
-
-            <Footer/>
-        </div>
-    )
-
+        )
+    }
 }
+
+export default withRouter(Signup_2)
